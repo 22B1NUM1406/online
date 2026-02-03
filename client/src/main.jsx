2a434e4aa030a7642271862577921071748d2a20
Manal123 <manal05116@gmail.com>
2026-02-03 11:41:24 +0800

    yadag hugiin pizda we

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-// client/src/main.jsx
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'

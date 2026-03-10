e24b847789ecb9274b49067de4918f47a151782f
Manal123 <manal05116@gmail.com>
2026-03-10 14:59:21 +0800

    hghaohgoiwhgoihgohwighwoi

--- client/src/components/AdminRedirectRoute.jsx
+++ client/src/components/AdminRedirectRoute.jsx
@@ -1,16 +1,16 @@
-// components/AdminRedirectRoute.jsx
+// src/components/AdminRedirectRoute.jsx
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
 const AdminRedirectRoute = ({ children }) => {
-  const { user } = useAuth();
+  const { isAdmin, loading } = useAuth();
 
-  // Хэрэв админ нэвтэрсэн бол dashboard руу шууд явуулна
-  if (user?.isAdmin) {
+  if (loading) return null;
+
+  if (isAdmin) {
     return <Navigate to="/admin/dashboard" replace />;
   }
 
-  // Нэвтрээгүй бол login хуудас харуулна
   return children;
 };
 
